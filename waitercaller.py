e77643fb042688cd7fc36e52748daa1ab5cdce73
al <milicevic.alain@free.fr>
2017-07-11 08:36:27 +0200

    initial

---
+++ waitercaller.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "under contstruction"
+
+if __name__ == '__main__':
+    app.run(port=8000, debut = True)
+
